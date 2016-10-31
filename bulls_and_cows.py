d607f4ff252ceca21f3b2d4d53e0b18891e1f7bc
Fetiu <k9380450@gmail.com>
2016-10-31 10:30:10 +0000

    https://github.com/Fetiu/bulls_and_cows

--- bulls_and_cows.py
+++ bulls_and_cows.py
@@ -15,25 +15,30 @@ def strike_check(pn_list,cn_list):
 					ball_cnt+=1
 		pn_cnt+=1
 	print('\n')
-	if not(strk_cnt or ball_cnt): return 1 #숫자 하나도 못맞출경우
-	if strk_cnt: print(strk_cnt,'스트라이크! '), #개행 없는 표준출력을 위해 ','사용
-	if ball_cnt: print(ball_cnt,'볼! ')
+	if not(strk_cnt or ball_cnt): print('아웃!') #숫자 하나도 못맞출경우
+	elif strk_cnt==3: print('삼진! 축하합니다!'); return 1
+	else:
+		if strk_cnt: print(strk_cnt,'스트라이크! '), #개행 없는 표준출력을 위해 ','사용
+		if ball_cnt: print(ball_cnt,'볼! ')
 	return 0
 
 """__main__"""
+loop_cnt=0
+pn_list=[0,0,0]
+num_data=[1,2,3,4,5,6,7,8,9]
+
+print('\n\n[<숫자 야구 게임>]\n')
+
+print('컴퓨터가 세개의 수를 정하고 있습니다.\n')
+cn_list= random.sample(num_data,3) #중복 순열
+sleep(1)
+	
 while True:
 	i=0
-	pn_list=[0,0,0]
-	cn_list=[0,0,0]
-	num_data=[1,2,3,4,5,6,7,8,9]
-	
-	print('\n\n[<숫자 야구 게임>]\n')
-	
-	print('컴퓨터가 세개의 수를 정하고 있습니다.\n')
-	cn_list= random.sample(num_data,3) #중복 순열
-	sleep(1)
-	
-	print('(힌트: 세 수의 합은',sum(cn_list),')\n')
+	loop_cnt+=1
+	print("\n~제", loop_cnt,'회 시작~')
+	if loop_cnt ==3:
+		print('(힌트: 세 수의 합은',sum(cn_list),')\n')
 	#print(cn_list,'\n')
 	while i!=3:
 		print(i+1,'번째 공 위치 선택: ')
@@ -41,6 +46,5 @@ while True:
 		i+=1
 		
 	if strike_check(pn_list,cn_list):
-		print("아웃!!")
-		
+		break	
 
